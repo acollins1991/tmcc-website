95acba5c09faf0b5207526cd6d49cb32e294672d
acollins1991 <a.r.c_@hotmail.co.uk>
2018-04-09 09:35:16 +0100

    Simplified banner markup and centered text

--- components/Homepage/Banner.vue
+++ components/Homepage/Banner.vue
@@ -1,13 +1,7 @@
 <template>
-<section class="uk-section uk-container uk-container-small" role="banner">
-  <div class="uk-grid-large uk-flex uk-flex-bottom" uk-grid>
-    <div class="uk-width-2-3">
-      <h1 class="uk-h2">Experts in Digital Marketing<br> and Public Relations</h1>
-    </div>
-    <div class="uk-width-1-3">
-      <p>We are a full-service agency based in Epsom, Surrey, UK.</p>
-    </div>
-  </div>
+<section class="uk-section uk-container uk-text-center" role="banner">
+  <h1>Experts in Digital Marketing<br> and Public Relations</h1>
+  <p>We are a full-service agency based in Epsom, Surrey, UK.</p>
 </section>
 </template>
 
