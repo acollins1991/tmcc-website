fdf90bf82e1612064a003eb7f068d1af54a52b82
acollins1991 <a.r.c_@hotmail.co.uk>
2018-04-13 14:06:16 +0100

    Reduced hide time on load

--- plugins/firstLoad.js
+++ plugins/firstLoad.js
@@ -1,10 +1,10 @@
 export default function ({ app }) {
   // on router ready, ie first load
   app.router.onReady(function() {
-    // delay by .5s
+    // delay by 1.5s
     setTimeout(function(){
       // remove opacity styling
       document.body.removeAttribute("style")
-    }, 3000)
+    }, 1500)
   })
 }
