d1578b290aa6ed194dca2e33638038db1fba4e50
acollins1991 <a.r.c_@hotmail.co.uk>
2018-05-23 16:51:51 +0100

    Responsive edits

--- components/Global/Footer.vue
+++ components/Global/Footer.vue
@@ -10,25 +10,25 @@
     </div>
 
     <div class="uk-flex">
-      <nuxt-link to='/about' class="uk-display-block uk-link-reset uk-width-2-5 uk-background-primary uk-padding uk-text-center">
+      <nuxt-link to="/about" class="uk-display-block uk-link-reset uk-width-2-5@s uk-width-1-1 uk-background-primary uk-padding uk-text-center">
         <span style="color:#ffffff;">About Us <span uk-icon="icon: arrow-right; ratio: 2;"></span></span>
       </nuxt-link>
-      <nuxt-link to='/blog' class="uk-display-block uk-link-reset uk-width-3-5 uk-padding uk-text-center" style="background-color:#52439C;">
+      <nuxt-link to="/blog" class="uk-display-block uk-link-reset uk-width-3-5@s uk-width-1-1 uk-padding uk-text-center" style="background-color:#52439C;">
         <span style="color:#ffffff;">Read more from our blog <span uk-icon="icon: arrow-right; ratio: 2;"></span></span>
       </nuxt-link>
     </div>
 
     <div class="uk-margin-top uk-flex uk-flex-right tmcc-font-montserrat" uk-grid>
-      <div class="uk-width-1-5">
+      <div class="uk-width-1-5@s uk-width-1-1">
         <h2 class="uk-h6 tmcc-font-montserrat uk-text-bold uk-text-uppercase">Hire</h2>
         <p class="uk-text-small">Want to create something with us?</p>
         <a class="uk-link-reset uk-text-small uk-text-bold tmcc-chat"><span uk-icon="icon: comment"></span> Chat with us</a>
       </div>
-      <div class="uk-width-2-5">
+      <div class="uk-width-2-5@s uk-width-1-1">
         <h2 class="uk-h6 tmcc-font-montserrat uk-text-bold uk-text-uppercase">Let's talk business</h2>
         <address class="uk-text-small">1b Kilmuir House, Depot Road, Epsom, Surrey, KT17 4RJ</address>
-        <a class="uk-link-reset uk-text-small uk-display-block" href="mailto:info@themarketingcampaigncopany.co.uk">info@themarketingcampaigncopany.co.uk</a>
-        <span class="uk-text-small uk-display-block">+44 (0) 1372 807189</span>
+        <a class="uk-link-reset uk-text-small uk-display-block" href="mailto:info@themarketingcampaigncopany.co.uk"><span uk-icon="icon: mail"></span> Email Us</a>
+        <span class="uk-text-small uk-display-block"><span uk-icon="icon: phone"></span> +44 (0) 1372 807189</span>
       </div>
     </div>
 
