7164d2a58ebe32206a36f44f4b734843edd0ee2c
acollins1991 <a.r.c_@hotmail.co.uk>
2018-05-25 11:58:30 +0100

    Removed separate home footer component as now conditional content

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,6 @@ import Banner from '../components/Homepage/Banner'
 import CaseStudies from '../components/Homepage/CaseStudies'
 
 export default {
-  layout: 'homepage',
   components: {
     Banner,
     CaseStudies
